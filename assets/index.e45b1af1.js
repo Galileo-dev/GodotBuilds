5cb87b78201ed0882f284728efe44da583abf1ee
Galileo-dev <Galileo-dev@users.noreply.github.com>
2021-06-28 12:39:06 +0000

    deploy: 8595a9ff9ade9cdf19967bd2aa7eae7eee22cf4f

---
+++ assets/index.e45b1af1.js
@@ -0,0 +1,100 @@
+import{q as e,a as n,N as t}from"./vendor.81b4ca82.js";function i(e){return n(o,null,n(r,null,n(d,null,n(l,null,"Download Godot Builds"),n(a,null," Unfortunately I have not made any builds for Linux or MacOs yet"),n(u,null,n(m,null,n(s,{src:"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Windows_logo_and_wordmark_-_2021.svg/250px-Windows_logo_and_wordmark_-_2021.svg.png"}),n(c,null,n(p,{href:"https://github.com/Galileo-dev/GodotBuilds/releases/download/Latest/Godot_Win_x64.exe"},"Latest Build")))))))}const o=e.section`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  height: auto;
+`,r=e.section`
+  width: 100%;
+  position: relative;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 80px 80px;
+`,a=e.p`
+  color: hsla(0, 0%, 95.3%, 1);
+  font-size: 0.9em;
+  margin: 0 0 24px;
+  line-height: 1.5em;
+  letter-spacing: 1.5px;
+`,l=e.h1`
+  font-size: 4rem;
+`,s=e.img`
+  margin: 12px;
+  max-width: 550px;
+  min-height: 1px;
+  display: block;
+  width: 10em;
+  transition-timing-function: ease-in;
+  transition: opacity 1s;
+  
+`,d=e.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  flex-direction: column;
+
+  justify-content: center;
+  margin-top: 0;
+  align-items: center;
+  text-align: center;
+  margin-right: auto;
+  margin-left: auto;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+  width: 100%;
+`,c=e.div`
+  display: inline;
+`,p=e.a`
+  display: inline-block;
+  font-weight: bold;
+  color: #f9f9f9;
+  border-radius: 35px;
+  background-color: #0063e5;
+  margin: 12px;
+  margin-top: 30px;
+  width: 9em;
+  border: 1px solid transparent;
+  letter-spacing: 1.5px;
+  font-size: 18px;
+
+  padding: 1em;
+  transition: all 0.2s ease 0s;
+
+  -webkit-touch-callout: none; /* iOS Safari */
+  -webkit-user-select: none; /* Safari */
+  -khtml-user-select: none; /* Konqueror HTML */
+  -moz-user-select: none; /* Old versions of Firefox */
+  -ms-user-select: none; /* Internet Explorer/Edge */
+  user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
+
+  &:hover {
+    background-color: #0483ee;
+    cursor: pointer;
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+`,m=e.div`
+  max-width: 650px;
+  flex-wrap: wrap;
+  display: flex;
+  margin-bottom: 50px;
+  flex-direction: column;
+  justify-content: center;
+  margin: 2em;
+  align-items: center;
+  text-align: center;
+  transition-timing-function: ease-out;
+  transition: opacity 0.2s;
+`,u=e.div`
+  background-color: #1a1d29;
+  border-radius: 33px;
+  border: 2px solid white;
+  align-items: center;
+  text-align: center;
+  margin-bottom: 20px;
+`;t(n(i,null),document.getElementById("app"));
